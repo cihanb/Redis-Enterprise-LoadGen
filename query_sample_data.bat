395d1ec0e6b7810088d069d14516c28e462dc0b3
cihanb <cihangirb@hotmail.com>
2016-09-30 11:35:57 -0700

    removed unnecessary files.

--- query_sample_data.bat
+++
@@ -1,2 +0,0 @@
-REM load 1M items with 100K value size with a1 having 100 distinct values, using 10 threads in parallel 
-%PYTHONPATH%\python.exe LoadGenRLEC.py -hn=redis-19063.rlec_azure.local:19063:redislabs123 -op=load -kp=A -ks=0 -ke=100 -vs=128 -sl=10 -loop=true
--- query_sample_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#load 1M items with 100K value size with a1 having 100 distinct values, using 10 threads in parallel 
-/usr/local/bin/python3.5 LoadGenRLEC.py -hn=redis-19063.rlec_azure.local:19063:redislabs123 -op=load -kp=A -ks=0 -ke=100 -vs=128 -sl=10 -loop=true
